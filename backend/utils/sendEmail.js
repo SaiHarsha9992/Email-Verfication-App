792b5e54ecb575f0a7dbd7e72603c9eb819f0ad4
Kedarinadh Sai Harsha <gaduharsha72@gmail.com>
2025-10-05 19:37:42 +0530

    changes in all

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -2,15 +2,17 @@ import nodemailer from "nodemailer";
 
 const sendEmail = async (to, subject, html) => {
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS,
     },
   });
 
   await transporter.sendMail({
-    from: `"Verify App" <${process.env.EMAIL_USER}>`,
+    from: `"Verify App" <${process.env.SMTP_USER}>`,
     to,
     subject,
     html,
