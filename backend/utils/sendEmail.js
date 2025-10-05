6916338d45178887ce290bc7d5bfe5ab3078c73a
Kedarinadh Sai Harsha <gaduharsha72@gmail.com>
2025-10-05 19:59:55 +0530

    last changes maybe

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -2,17 +2,17 @@ import nodemailer from "nodemailer";
 
 const sendEmail = async (to, subject, html) => {
   const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
+    host: "smtp.mailgun.org", // Mailgun SMTP server
     port: 587,
-    secure: false,
+    secure: false, // TLS
     auth: {
-      user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASS,
+      user: process.env.MG_USER, // postmaster@sandboxXXXX.mailgun.org
+      pass: process.env.MG_PASS, // Mailgun SMTP password
     },
   });
 
   await transporter.sendMail({
-    from: `"Verify App" <${process.env.SMTP_USER}>`,
+    from: `"Verify App" <${process.env.MG_USER}>`,
     to,
     subject,
     html,
